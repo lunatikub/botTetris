391bc11fd3ba1c018c1e12a917eaa55e04da4e9e
Thomas Joly <thomas.joly@scality.com>
2021-03-02 16:17:58 +0100

    add test get_line_well

--- tests/test_field.c
+++ tests/test_field.c
@@ -5,30 +5,12 @@
 #define HEIGHT 20
 #define WIDTH  10
 
-static void dump(struct field *field)
-{
-  for (uint8_t y = 0; y < field->height; y++) {
-    printf("(%2u) ", y);
-    for (uint8_t x = 0; x < field->width; x++) {
-      printf("%2u ", field->blocks[y][x]);
-    }
-    printf(" [%u]\n", field->line[y]);
-  }
-  printf("\n     ");
-  for (uint8_t x = 0; x < field->width; x++) {
-    printf("%2u ", field->col[x]);
-  }
-  printf("\n");
-}
-
 TEST_F(get_line)
 {
   struct field *f = field_new(HEIGHT, WIDTH);
 
   SET(f, 18, LINE({0, 1, 0}));
   SET(f, 19, LINE({1, 1, 1}));
-
-  dump(f);
 
   const struct rotation *r = rotation_get(TETRIMINO_J, 1);
 
@@ -47,8 +29,28 @@ TEST_F(get_line)
   return true;
 }
 
+TEST_F(get_line_well)
+{
+  struct field *f = field_new(HEIGHT, WIDTH);
+
+  SET(f, 16, LINE({1, 0, 1}));
+  SET(f, 17, LINE({1, 0, 1}));
+  SET(f, 18, LINE({1, 0, 1}));
+  SET(f, 19, LINE({1, 0, 1}));
+
+  const struct rotation *r = rotation_get(TETRIMINO_I, 1);
+
+  EXPECT_EQ(get_line(f, r, 0), 16);
+  EXPECT_EQ(get_line(f, r, 1), 20);
+  EXPECT_EQ(get_line(f, r, 2), 16);
+
+  field_destroy(f);
+  return true;
+}
+
 const static struct test field_tests[] = {
   TEST(get_line),
+  TEST(get_line_well),
 };
 
 TEST_SUITE(field);
